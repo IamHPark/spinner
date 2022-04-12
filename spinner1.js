d13a4c5a36fb2922d24bf0a19c96c410417a006e
IamHPark <heesoo.p1023@gmail.com>
2022-04-12 16:48:48 +0000

    initial work for spinner1

---
+++ spinner1.js
@@ -0,0 +1,27 @@
+// process.stdout.write("hello from spinner1.js...\rheyyy\n")
+let time = 100;
+while(time < 5000) {
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, time);
+time += 200;
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, time);
+time += 200;
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, time);
+time += 200;
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, time);
+time += 200;
+};
+
+setTimeout(() => {
+  console.log('\n');
+}, time);
